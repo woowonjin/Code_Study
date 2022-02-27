4c0631945ea42683565f9deb3c6cc72536cf9d92
woowonjin <dndnjswls613@naver.com>
2022-02-27 21:37:44 +0900

    [BOJ] / 거짓말 / gold 4 / 50분 / #46

--- WONJIN/Week10/lie.py
+++ WONJIN/Week10/lie.py
@@ -1,4 +1,13 @@
 N, M = list(map(int, input().split()))
-known_person = list(map(int, input().split()))
-known_person_num = known_person[0]
-known_persons = known_person[1:]
+
+true_person = set(list(map(int, input().split()))[1:])
+parties = [set(list(map(int, input().split()))[1:]) for _ in range(M)]
+
+lie_list = [1]*M
+
+for _ in range(M):
+    for idx, party in enumerate(parties):
+        if true_person.intersection(party):
+            lie_list[idx] = 0
+            true_person = true_person.union(party)
+print(sum(lie_list))
